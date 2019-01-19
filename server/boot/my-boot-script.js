9eb20f9756d5a8bab182aed767ae3e729be93c1e
Bal Krishn Singh <balkrishn.singh@sojern.com>
2019-01-19 20:40:56 +0530

    add two more users

--- server/boot/my-boot-script.js
+++ server/boot/my-boot-script.js
@@ -1,7 +1,24 @@
 module.exports = function(app) {
     var User = app.models.User;
-    User.create({ email: "usertest123456@gmail.com", password: "test123456" }, function(err, userInstance) {
-      console.log('Test user created... You can use this user credentials for testing..');
+    let user_list = [
+      {
+        email: 'yourock@shoppinpal.com',
+        password: 'test123'
+      },
+      {
+        email: 'goodjob@shoppinpal.com',
+        password: 'test123'
+      },
+      {
+        email: "usertest123456@gmail.com",
+        password: "test123456"
+      }
+    ];
+
+    User.create(user_list, function(err, userInstance) {
+      console.log('Test user created... You can use this user login credentials for testing..');
       console.log("{ email: usertest123456@gmail.com, password: test123456 }");
+      console.log("{ email: goodjob@shoppinpal.com, password: test123 }");
+      console.log("{ email: yourock@shoppinpal.com, password: test123 }");
     });
 }
